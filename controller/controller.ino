a43317b37fac8ba44a56836dac5489fbacc7ab63
shanshan <72496903+shananshan@users.noreply.github.com>
2024-11-16 18:45:45 +0800

    Update controller.ino

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,10 +1,13 @@
 #include "LineSensors.h"
-#include "Motors.h"
+//#include "Motors.h"
 #include "Button.h"
+#include <Pololu3piPlus32U4.h>
 #include <math.h>
 #include <PololuOLED.h>
 #include <EEPROM.h>
 #include <Wire.h>
+
+using namespace Pololu3piPlus32U4;
 
 PololuSH1106 display(1, 30, 0, 17, 13);
 
@@ -13,7 +16,8 @@ PololuSH1106 display(1, 30, 0, 17, 13);
 #define MOVING (0)
 #define STOP (1)
 
-Motors_c motors;
+//Motors_c motors;
+Motors motors;
 LineSensors_c line_sensors;
 Button_c button;
 
@@ -30,8 +34,8 @@ unsigned long set_wheel_duration = 20;
 unsigned long set_wheel_end = 0;
 float left_wheel = 0.0f;
 float right_wheel = 0.0f;
-float lspeed = 20.0f;
-float rspeed = 20.0f;
+float lspeed = 40.0f;
+float rspeed = 40.0f;
 
 unsigned long state = MOVING;
 
@@ -41,19 +45,20 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   // initialise
   Wire.begin();
-  motors.initialise();
+//  motors.initialise();
   line_sensors.initialiseForADC();
   button.initialiseForButton();
 
   // calibrate
   digitalWrite(LED_BUILTIN, HIGH);
-  motors.setPWM(lspeed,rspeed);
+//  motors.setPWM(lspeed,rspeed);
+  motors.setSpeeds(lspeed,rspeed);
   line_sensors.calibrateSensors();
   digitalWrite(LED_BUILTIN, LOW);
-  motors.setPWM(0,0);
+//  motors.setPWM(0,0);
+  motors.setSpeeds(0,0);
   line_sensors.setDestination();
   Serial.println("******************************************");
-//  motors.setPWM(0,0);
 
   display.init();
   display.clear();
@@ -61,7 +66,7 @@ void setup() {
   // show the calibration data
   displaycali();
 
-  delay(5000);
+  delay(8000);
   display.clear();
   start_millis = millis();
 }
@@ -74,20 +79,22 @@ void loop() {
   if (state == MOVING){
     if (line_sensors.arrived(2)){
       displaySensorData();
-//      digitalWrite(LED_BUILTIN, HIGH);
       Serial.println("Arrived");
       setStop();  
       state = STOP; 
-    }
-    else {
+    } else {
       digitalWrite(LED_BUILTIN, HIGH);
       Serial.println("Not arrived");
       setWheel(lspeed,rspeed);
       displayMoving();
     }
   }else{
-      Serial.println("Unknown state or STOP");
+    if (button.AisPressed()){
+        state = MOVING;
+    }
+    Serial.println("Unknown state or STOP");
   }
+  
 //  displaySensorData();
   display.display();
 }
@@ -110,7 +117,8 @@ void setWheel(float x, float y) {
 
 void checkWheel() {
    if (millis() > set_wheel_end) {
-       motors.setPWM(left_wheel, right_wheel);
+//       motors.setPWM(left_wheel, right_wheel);
+       motors.setSpeeds(left_wheel, right_wheel);
        setWheel(left_wheel, right_wheel);
        set_wheel_end = millis() + set_wheel_duration;
    }
@@ -121,27 +129,30 @@ void displayMoving() {
   display.print("sensor2......");
   display.gotoXY(0, 1);
   display.println(line_sensors.getcalibrated(2));
+  display.gotoXY(0, 2);
+  display.println(line_sensors.getpercentage());
 //  display.display();
 }
 void displaySensorData() {
-  display.setLayout8x2();
+  
+  display.setLayout11x4();
   display.gotoXY(0, 0);
   display.print("sensor2");
   display.gotoXY(0, 1);
   display.println(line_sensors.getcalibrated(2));
+  display.gotoXY(0, 2);
+  display.println(line_sensors.getpercentage());
 //  display.display();
 }
 void displaycali(){
   display.setLayout11x4();
   display.gotoXY(0, 0);
-  display.print("min:");
-  display.println(line_sensors.getmin(2));
+  display.print("min:");  display.println(line_sensors.getmin(2));
   display.gotoXY(0, 1);
-  display.print("max:");
-  display.println(line_sensors.getmax(2));
+  display.print("max:");  display.println(line_sensors.getmax(2));
   display.gotoXY(0, 2);
-  display.print("destination");
+  display.print("D:");  display.println(line_sensors.getdes());
   display.gotoXY(0, 3);
-  display.println(line_sensors.getdes());
+  display.print("P:");  display.println(line_sensors.getpercentage());
 //  display.display();
 }
